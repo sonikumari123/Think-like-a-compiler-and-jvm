12e9a3dfe27812cce685646575409fae2787d622
soni kumari <sonykumarinaiya.sk@gmail.com>
2018-08-01 22:46:04 +0530

    code for operators
    Think like a compiler and jvm

---
+++ Think like a CompilereAndJVM.java
@@ -0,0 +1,33 @@
+class ThinkCompilereAndJVM{
+	public static void main(String[]args){
+	System.out.println(10);
+	System.out.println('a');
+	System.out.println("a");
+	System.out.println(10.0);
+	System.out.println(10.345f);
+	System.out.println(30L);
+	System.out.println(30l);
+	System.out.println(50+20);
+int a=30;
+int b=40;
+	System.out.println(a+b);
+	System.out.println("a+b");
+	System.out.println("a+b"+a+b);
+	System.out.println("a+b"+(a+b));
+	//System.out.println("a-b"+a-b);
+	System.out.println("a-b"+(a-b));
+	System.out.println("a*b"+a*b);
+	System.out.println("a*b"+(a*b));
+	System.out.println(""+10+20);
+	System.out.println(10+""+20);
+	System.out.println(22F/7*10*10);
+	System.out.println(22D/7*10*10);
+	//System.out.println(10/0);
+	System.out.println(10.0/0);
+	System.out.println(-10.0/0);
+	//System.out.printf(0/0);
+	//System.out.println(0.0/0);
+	System.out.print("hi"+5+9);
+	System.out.println(-0.0/0);
+	}
+}
